22b42e48ec7aaaa08eeadadb2bc4eaa414469ac9
raihara3 <raihara3@gmail.com>
2020-12-18 09:24:22 +0900

    :sparkles: get bluetooth acceptAllDevice

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,12 @@
-const main = () => {}
+const main = async () => {
+  const bluetooth = window.navigator.bluetooth;
+  const device = await bluetooth.requestDevice({
+    acceptAllDevices: true
+  })
+  console.log(device)
+}
+
+const button = document.getElementById('bluetooth')
+if(button) {
+  button.addEventListener('click', main)
+}
