433945aa1a80c334684151a83d12024568b0b8d1
raihara3 <raihara3@gmail.com>
2020-12-19 17:41:38 +0900

    :sparkles: Get the distance between devices

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,36 @@
-const main = async () => {
-  const bluetooth = window.navigator.bluetooth;
+let deviceName = ''
+
+const pairing = async () => {
+  const bluetooth = window.navigator.bluetooth
   const device = await bluetooth.requestDevice({
     acceptAllDevices: true
   })
-  console.log(device)
+  console.debug(`name: ${device.name} ,id: ${device.id}`)
+  deviceName = device.name ? device.name : ''
 }
 
+const main = async () => {
+  const bluetooth = window.navigator.bluetooth
+
+  await bluetooth.requestLEScan({
+    acceptAllAdvertisements: true
+  })
+
+  bluetooth.addEventListener('advertisementreceived', event => {
+    if(deviceName === event.device.name) {
+      const d = Math.pow(10.0, (event.txPower - (event.rssi)) / 17.0)
+
+      const distance = document.getElementById('distance')
+      if(distance) {
+        distance.innerText = `${event.device.name}までの距離は${Math.round(d / 100)}cm`
+      }
+    }
+  })
+}
+
+const pairingButton = document.getElementById('pairing')
 const button = document.getElementById('bluetooth')
-if(button) {
+if(pairingButton && button) {
+  pairingButton.addEventListener('click', pairing)
   button.addEventListener('click', main)
 }
